71df7f480e4d342d711e5d28ef61a64d546aeeab
raingloom <raingloom42@gmail.com>
2017-06-18 15:33:33 +0200

    emacs stuffz

--- .emacs.d/elpa/pdb-mode-20150128.951/pdb-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pdb-mode" "20150128.951" "Major mode for editing Protein Data Bank files" 'nil :url "http://bondxray.org/software/pdb-mode/" :keywords '("data" "pdb"))
